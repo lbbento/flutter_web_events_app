9aa13616af2d82eb496399185a1d162170248c06
Lucas Bento <lbento@tyro.com>
2019-05-29 23:00:44 +1000

    Created dialog

---
+++ lib/events/add/eventadddialog.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:unisinos_events/db/database_helper.dart';
+import 'package:unisinos_events/events/event.dart';
+
+class EventAddDialog {
+  
+  final teTitle = TextEditingController();
+  final teDescription = TextEditingController();
+  final teDate = TextEditingController();
+  Event event;
+
+  static const TextStyle linkStyle = const TextStyle(
+    color: Colors.blue,
+    decoration: TextDecoration.underline,
+  );
+
+  Widget buildAboutDialog(
+      BuildContext context, _myEventPageState, Event event) {
+    if (event != null) {
+      this.event=event;
+      teTitle.text = event.title;
+      teDescription.text = event.description;
+      teDate.text = event.date;
+    }
+
+    return new AlertDialog(
+      title: new Text('Add new Event'),
+      content: new SingleChildScrollView(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            getTextField("Enter title", teTitle),
+            getTextField("Enter descriptiom", teDescription),
+            getTextField("DD-MM-YYYY", teDate),
+            new GestureDetector(
+              onTap: () {
+                addRecord();
+                _myEventPageState.displayRecord();
+                Navigator.of(context).pop();
+              },
+              child: new Container(
+                margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                child: getAppBorderButton(
+                    "Add", EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getTextField(
+      String inputBoxName, TextEditingController inputBoxController) {
+    var loginBtn = new Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: new TextFormField(
+        controller: inputBoxController,
+        decoration: new InputDecoration(
+          hintText: inputBoxName,
+        ),
+      ),
+    );
+
+    return loginBtn;
+  }
+
+  Widget getAppBorderButton(String buttonLabel, EdgeInsets margin) {
+    var loginBtn = new Container(
+      margin: margin,
+      padding: EdgeInsets.all(8.0),
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        border: Border.all(color: const Color(0xFF28324E)),
+        borderRadius: new BorderRadius.all(const Radius.circular(6.0)),
+      ),
+      child: new Text(
+        buttonLabel,
+        style: new TextStyle(
+          color: const Color(0xFF28324E),
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      ),
+    );
+    return loginBtn;
+  }
+
+  Future addRecord() async {
+    var db = new DatabaseHelper();
+    var event = new Event(title: teTitle.text, description: teDescription.text, date: teDate.text);
+    await db.saveEvent(event);
+  }
+}
